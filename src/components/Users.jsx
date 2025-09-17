0fe4df1db0a2b109e7019d06c6ebc7223c3300da
shahadot99999 <shahadot99999@gmail.com>
2025-09-17 07:45:58 +0600

    clent side  delete  method apply. deleted one by one data

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,11 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 
 const Users = ({usersPromise}) => {
 
     const initalUsers = use(usersPromise);
-    console.log(initalUsers);;
+    console.log(initalUsers);
+
+    const [users, setUsers]= useState(initalUsers)
 
     const handleAddUser = e=>{
         e.preventDefault();
@@ -25,15 +27,36 @@ const Users = ({usersPromise}) => {
         .then(data =>{
             console.log('data after creating user in db', data);
             if(data.insertedId){
+                newUser._id = data.insertedId;
+                const newsUsers = [...users, newUser];
+                setUsers(newsUsers);
                 alert('User added successfully');
                 e.target.reset();
             }
+        })
+    }
+
+    const handleUserDelete = (id)=>{
+        console.log('delete this user', id);
+        fetch(`http://localhost:3000/users/${id}`,{
+            method: "DELETE"
+
+        } )
+        .then(res => res.json())
+        .then(data =>{
+            if(data.deletedCount){
+                const remainingUsers = users.filter(user => user._id !==id)  ;
+                setUsers(remainingUsers);
+                console.log('after deleted', data);
+            }
+            // console.log('after deleted', data);
         })
     }
     return (
         <div>
             {/* Add uers */}
             <div>
+                <h4>Users: {users.length}</h4>
                 <form onSubmit={handleAddUser}>
                     <input type="text" name='name' />
                     <br />
@@ -41,6 +64,16 @@ const Users = ({usersPromise}) => {
                     <br />
                     <input type="submit" value="Add User" />
                 </form>
+            </div>
+            {/* view users */}
+            <div>
+                {
+                    users.map(user => <p
+                         key={user._id}>
+                            {user.name} : {user.email}
+                            <button onClick={()=>handleUserDelete(user._id)}>X</button>
+                            </p>)
+                }
             </div>
         </div>
     );
