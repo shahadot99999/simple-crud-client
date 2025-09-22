c5fc79537ed66338499f19d4e7334eb861ef5519
shahadot99999 <shahadot99999@gmail.com>
2025-09-22 15:49:30 +0600

    update client side is completed

--- src/components/UpdateUser.jsx
+++ src/components/UpdateUser.jsx
@@ -9,7 +9,25 @@ const UpdateUser = () => {
         e.preventDefault();
         const name = e.target.name.value;
         const email = e.target.email.value;
-        console.log(name, email);
+        const updatedUser = {name, email}
+        console.log(updatedUser);
+
+        //update user info inthe db
+        fetch(`http://localhost:3000/users/${user._id}`, {
+            method: 'PUT', 
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount){
+                 console.log(' update done ', data)
+            }
+            
+
+        })
     }
 
     return (
